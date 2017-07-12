de6a31c6e859d6c211573e7892ba70ce646a344c
MohamedSellahi <mohamed.sellahi@gmail.com>
2017-07-12 16:50:15 +0200

    InitMVC

---
+++ PartyIvites/PartyIvites/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PartyIvites.Controllers {
+   public class HomeController : Controller {
+
+     
+      // GET: Home
+      public ActionResult Index() {
+
+         // passing some data to the view 
+         int hour = DateTime.Now.Hour;
+         ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
+         return View();
+      }
+
+   }
+}
