c0d81c99ffe1db2e629503f89efa259b76aae389
MohamedSellahi <mohamed.sellahi@gmail.com>
2017-07-13 09:53:45 +0200

    Add simple validation rules for for Forms

--- PartyIvites/PartyIvites/Controllers/HomeController.cs
+++ PartyIvites/PartyIvites/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PartyIvites.Models;
 
 namespace PartyIvites.Controllers {
    public class HomeController : Controller {
@@ -17,6 +18,28 @@ namespace PartyIvites.Controllers {
          ViewBag.Message = "Here is another message";
          return View();
       }
+
+      // HAndeling get requested from the RsvpForm 
+      [HttpGet]
+      public ActionResult RsvpForm() {
+         return View();
+      }
+
+      // Handeling Post requests from RsvpForm 
+      [HttpPost]
+      public ActionResult RsvpForm(GuestResponse guestresponse) {
+         if (ModelState.IsValid) {
+         // TODO: Email response to the party organizer 
+         return View("Thanks",guestresponse);
+         }
+         else {
+            // there is a validation error
+            // return to the Form view 
+            return View();
+         }
+      }
+
+
 
    }
 }
