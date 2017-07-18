ef850ca6059a1dd71f5a9110144a96d3f8ea4da9
Rémi GESTIN <rgestin@proxiel.local>
2017-07-18 16:48:49 +0200

    redo first project

---
+++ PI/PI/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PI.Models;
+
+namespace PI.Controllers {
+    public class HomeController : Controller {
+        // GET: Home
+        public ViewResult Index() {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good afternoon";
+            return View();
+        }
+
+
+        // to be called for get requests 
+        [HttpGet]
+        public ViewResult RsvpForm() {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponce guestResponce) {
+            // todo: Email responce to party organizer 
+            // use validation rules 
+            if(ModelState.IsValid)
+            {
+                return View("Thanks", guestResponce);
+            }
+            else
+            {
+                // data not valid: display form 
+                return View();
+            }
+        }
+    }
+
+
+}
