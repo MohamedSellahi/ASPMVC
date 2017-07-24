60a084820b9d3f34b4c5ec332e624eca5070252d
MohamedSellahi <mohamed.sellahi@gmail.com>
2017-07-24 21:17:26 +0200

    init razor

---
+++ Razor/Razor/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Razor.Models;
+
+namespace Razor.Controllers {
+   public class HomeController : Controller {
+
+      Product _myProduct = new Product {
+         Name = "Kayak",
+         Category = "Watersport",
+         Price = 275M,
+         Description = "a boat for one person ",
+         ProductID = 1
+      };
+
+      // GET: Home
+      public ActionResult Index() {
+         return View(_myProduct);
+      }
+   }
+}
