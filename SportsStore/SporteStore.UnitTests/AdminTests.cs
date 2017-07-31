dc7b1bb778a9c01d4a14e410622f517ed3d2f9e7
MohamedSellahi <mohamed1984>
2017-07-31 17:22:32 +0200

    Admin CRUD functionnality. Init

---
+++ SportsStore/SporteStore.UnitTests/AdminTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SportsStore.Domain.Entities;
+using SportsStore.Domain.Abstract;
+using SportsStore.WebUI.Controllers;
+using System.Linq;
+using System.Web.Mvc;
+using System.Collections.Generic;
+
+namespace SportsStore.UnitTests {
+   [TestClass]
+   public class AdminTests {
+
+
+      [TestMethod]
+      public void Index_Contains_All_Products() {
+         // Arrange - create the mock repo 
+         Mock<IProductRepository> mock = new Mock<IProductRepository>();
+         mock.Setup(m => m.Products).Returns(new Product[] {
+            new Product {ProductID = 1, Name = "P1"},
+            new Product {ProductID = 2, Name = "P2"},
+            new Product {ProductID = 3, Name = "P3"},
+         });
+
+         // Arrange create  acontroller 
+         AdminController target = new AdminController(mock.Object);
+
+         // Assert
+         Product[] result = ((IEnumerable<Product>)target.Index().ViewData.Model).ToArray();
+
+         // Assert 
+         Assert.AreEqual(result.Length, 3);
+         Assert.AreEqual(result[0].Name, "P1");
+         Assert.AreEqual(result[1].Name, "P2");
+         Assert.AreEqual(result[2].Name, "P3");
+
+      }
+
+
+
+
+   }
+}
