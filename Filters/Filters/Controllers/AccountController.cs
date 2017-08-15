d04a94d279cda07b7a90323f52c997cfe1a83c66
Mohamed <mohamed.sellahi@gmail.com>
2017-08-16 00:17:52 +0200

    Filters Init

---
+++ Filters/Filters/Controllers/AccountController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace Filters.Controllers {
+   public class AccountController : Controller {
+
+      public ActionResult Login() {
+         return View();
+      }
+
+      [HttpPost]
+      public ActionResult Login(string username, string password, string returnUrl) {
+         bool result = FormsAuthentication.Authenticate(username, password);
+         if (result) {
+            FormsAuthentication.SetAuthCookie(username, true);
+            //return Redirect(returnUrl ?? Url.Action("Index", "Admin"));
+            return Redirect(Url.Action("Index", "Admin"));
+         }
+         else {
+            ModelState.AddModelError("", "Incorrect username or password");
+            return View();
+         }
+      }
+
+
+
+   }
+}
---
+++ Filters/Filters/Controllers/AdminController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Filters.Controllers {
+   public class AdminController : Controller {
+
+      // GET: Admin
+      public string Index() {
+         return "This is the Index method of Admin controller";
+      }
+   }
+}
