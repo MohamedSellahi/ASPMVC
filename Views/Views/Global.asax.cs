070b525d8b289becfb84cc6d5b806cfd0d5f727a
MohamedSellahi <mohamed1984>
2017-08-22 14:29:23 +0200

    Views

---
+++ Views/Views/Global.asax.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Views.Infrastructre;
+
+namespace Views {
+   public class MvcApplication : System.Web.HttpApplication {
+      protected void Application_Start() {
+         AreaRegistration.RegisterAllAreas();
+         RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+         // registering my custom ViewEngine
+         ViewEngines.Engines.Insert(0,new DebugDataViewEngine());
+      }
+   }
+}
