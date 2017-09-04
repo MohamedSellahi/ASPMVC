70a6c4ccc05e7229c01d3d3760b20e08980e4990
MohamedSellahi <mohamed1984>
2017-09-04 17:23:04 +0200

    Model Binding Init

---
+++ MvcModels/MvcModels/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using MvcModels.Models;
+
+namespace MvcModels.Controllers {
+   public class HomeController : Controller {
+      private Person[] personData = {
+         new Person {PersonId = 1, FirstName = "Adam", LastName = "Freeman", Role = Role.Admin},
+         new Person {PersonId = 2, FirstName = "Jacqui", LastName = "Griffyth",Role = Role.User},
+         new Person {PersonId = 3, FirstName = "John", LastName = "Smith", Role = Role.User},
+         new Person {PersonId = 4, FirstName = "Anne", LastName = "Jones", Role = Role.Guest}
+         };
+
+
+      // GET: Home
+      // setting nullable parametter provides a fallback mechanism to the model binder 
+      public ActionResult Index(int id = 1) {
+         Person dataItem = personData.Where(p => p.PersonId == id).First();
+         return View(dataItem);
+      }
+
+      // Binding complex data 
+      public ActionResult CreatePerson() {
+         return View(new Person());
+      }
+
+      [HttpPost]
+      public ActionResult CreatePerson(Person model) {
+         return View("Index", model);
+      }
+
+      // use the bind attribute to tell MVC how to partially bind 
+      // the model
+      // [Bind(Prefix ="HomeAddress", Exclude ="Country")]
+      public ActionResult DisplaySummary([Bind(Prefix ="HomeAddress")]AddressSummary summary) {
+         return View(summary);
+      }
+   }
+}
---
+++ MvcModels/MvcModels/Models/AddressSummary.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcModels.Models {
+   // specify the properties on which we allow binding 
+   [Bind(Include = "City")]
+   public class AddressSummary {
+      public string City { get; set; }
+      public string Country { get; set; }
+   }
+}
