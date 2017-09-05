1dab76757aa318ace4decb6f6c50080e27911776
MohamedSellahi <mohamed1984>
2017-09-05 13:00:25 +0200

    CustomValueProvider

--- MvcModels/MvcModels/Global.asax.cs
+++ MvcModels/MvcModels/Global.asax.cs
@@ -4,15 +4,16 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using MvcModels.Infrastructre;
 
-namespace MvcModels
-{
-    public class MvcApplication : System.Web.HttpApplication
-    {
-        protected void Application_Start()
-        {
-            AreaRegistration.RegisterAllAreas();
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
-        }
-    }
+namespace MvcModels {
+   public class MvcApplication : System.Web.HttpApplication {
+
+      protected void Application_Start() {
+         AreaRegistration.RegisterAllAreas();
+         RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+         ValueProviderFactories.Factories.Insert(0, new CustomValueProviderFactory());
+      }
+   }
 }
---
+++ MvcModels/MvcModels/Infrastructre/CountryValueProvider.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+using System.Web.ModelBinding;
+
+namespace MvcModels.Infrastructre {
+   public class CountryValueProvider : IValueProvider {
+
+      public bool ContainsPrefix(string prefix) {
+         return prefix.ToLower().IndexOf("country") > -1;
+      }
+
+      public ValueProviderResult GetValue(string key) {
+         if (ContainsPrefix(key)) {
+            return new ValueProviderResult("USA", "USA", CultureInfo.InvariantCulture);
+         }
+         else {
+            return null;
+         }
+      }
+   }
+}
