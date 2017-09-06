e646176710a175f28ab44df43a46dae47a0ddd11
Mohamed <mohamed.sellahi@gmail.com>
2017-09-06 23:58:16 +0200

    Model Validation

---
+++ ModelValidation/ModelValidation/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using ModelValidation.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ModelValidation.Controllers {
+   public class HomeController : Controller {
+      public ViewResult makeBooking() {
+         return View(new Appointment() { Date = DateTime.Now });
+      }
+
+      [HttpPost]
+      public ViewResult makeBooking(Appointment appt) {
+         //Explicite validation of the model 
+         if (string.IsNullOrEmpty(appt.ClientName)) {
+            ModelState.AddModelError("ClientName", "Please enter your name");
+         }
+
+         if (ModelState.IsValidField("Date") && DateTime.Now > appt.Date) {
+            ModelState.AddModelError("Date", "Plase enter a date in the future");
+         }
+
+         if (!appt.TermsAccepted) {
+            ModelState.AddModelError("TermsAccepted", "You must accept the terms");
+         }
+         if (ModelState.IsValid) {
+            // Store new appointment here 
+            return View("Completed", (object)appt);
+         }
+         else {
+            return View();
+         }
+      }
+
+      // GET: Home
+      //public ActionResult Index() {
+      //   return View();
+      //}
+   }
+}
---
+++ ModelValidation/ModelValidation/Models/Appointment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ModelValidation.Models {
+   public class Appointment {
+      public string ClientName { get; set; }
+
+      [DataType(DataType.DateTime)]
+      public DateTime Date { get; set; }
+
+      [UIHint("Boolean")]
+      public bool TermsAccepted { get; set; }
+
+      //[UIHint("Boolean2")]
+      //public bool TermsAccepted2 { get; set; }
+   }
+}
