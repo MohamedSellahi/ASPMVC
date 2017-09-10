2a9e8d24feef1f57dac4b90a27ae0ff310053926
Mohamed <mohamed.sellahi@gmail.com>
2017-09-10 18:52:50 +0200

    End ModelValidation

--- ModelValidation/ModelValidation/Models/Appointment.cs
+++ ModelValidation/ModelValidation/Models/Appointment.cs
@@ -1,6 +1,57 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
 using ModelValidation.Infrastructure;
+using System.Web.Mvc;
+
+//namespace ModelValidation.Models {
+
+//   [NoJoeOnMondays]
+//   public class Appointment {
+
+//      [Required]
+//      public string ClientName { get; set; }
+
+//      [DataType(DataType.DateTime)]
+//      //[Required(ErrorMessage ="Please enter a date")]
+//      [FutureDate(ErrorMessage ="Please enter a date in the future")]
+//      public DateTime Date { get; set; }
+
+//      //[Range(typeof(bool),"true", "true",ErrorMessage ="You must accept the termes")]
+//      [MustBeTrue(ErrorMessage ="You must accept the terms")]
+//      [UIHint("Boolean")]
+//      public bool TermsAccepted { get; set; }
+
+//      //[UIHint("Boolean2")]
+//      //public bool TermsAccepted2 { get; set; }
+//   }
+//}
+
+
+   // Self validation version
+//namespace ModelValidation.Models {
+
+//   [NoJoeOnMondays]
+//   public class Appointment {
+
+//      [Required]
+//      public string ClientName { get; set; }
+
+//      [DataType(DataType.DateTime)]
+//      //[Required(ErrorMessage ="Please enter a date")]
+//      [FutureDate(ErrorMessage = "Please enter a date in the future")]
+//      public DateTime Date { get; set; }
+
+//      //[Range(typeof(bool),"true", "true",ErrorMessage ="You must accept the termes")]
+//      [MustBeTrue(ErrorMessage = "You must accept the terms")]
+//      [UIHint("Boolean")]
+//      public bool TermsAccepted { get; set; }
+
+//      //[UIHint("Boolean2")]
+//      //public bool TermsAccepted2 { get; set; }
+//   }
+//}
+
+//Client side validation examples 
 
 namespace ModelValidation.Models {
 
@@ -8,16 +59,13 @@ namespace ModelValidation.Models {
    public class Appointment {
 
       [Required]
+      [StringLength(10,MinimumLength =3)]
       public string ClientName { get; set; }
 
       [DataType(DataType.DateTime)]
-      //[Required(ErrorMessage ="Please enter a date")]
-      [FutureDate(ErrorMessage ="Please enter a date in the future")]
+      [Remote("validateDate","Home")]
       public DateTime Date { get; set; }
 
-      //[Range(typeof(bool),"true", "true",ErrorMessage ="You must accept the termes")]
-      [MustBeTrue(ErrorMessage ="You must accept the terms")]
-      [UIHint("Boolean")]
       public bool TermsAccepted { get; set; }
 
       //[UIHint("Boolean2")]
