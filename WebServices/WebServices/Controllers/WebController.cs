253a069efe000de02b2cd175bec2e88464b54d38
Mohamed <mohamed.sellahi@gmail.com>
2017-09-21 20:41:09 +0200

    webApi test0

---
+++ WebServices/WebServices/Controllers/WebController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using System.Web.Http;
+using WebServices.Models;
+
+
+namespace WebServices.Controllers {
+   public class WebController : ApiController {
+      private ReservationRepository repo = ReservationRepository.Curent;
+
+      public IEnumerable<Reservation> getAllResevations() {
+         return repo.GetAll();
+      }
+
+      public Reservation getReservation(int id) {
+         return repo.Get(id);
+      }
+
+      public Reservation postReservation(Reservation item) {
+         return repo.Add(item);
+      }
+
+      public bool putReservation(Reservation item) {
+         return repo.Update(item);
+      }
+
+      public void deleteReservation(int id) {
+         repo.Remove(id);
+      }
+      
+   }
+}
