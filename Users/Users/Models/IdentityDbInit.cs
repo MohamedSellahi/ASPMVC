e365c3c95b95f361ef65eff00253b01229b0ada8
Mohamed <mohamed.sellahi@gmail.com>
2018-08-08 08:20:58 +0200

    Identity revisited inti

---
+++ Users/Users/Models/IdentityDbInit.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace Users.Models
+{
+   public class IdentityDbInit : DropCreateDatabaseIfModelChanges<AppIdentityContext>
+   {
+      public override void InitializeDatabase(AppIdentityContext context)
+      {
+         PerformeInitialSetup(context);
+         base.Seed(context);
+      }
+      /// <summary>
+      /// performs initial setup 
+      /// </summary>
+      /// <param name="context"></param>
+      private void PerformeInitialSetup(AppIdentityContext context)
+      {
+         // init will go here 
+      }
+   }
+}
