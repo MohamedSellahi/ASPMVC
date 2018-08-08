e886f9da5c5f1bfa1efc46ca668c137151376421
Mohamed <mohamed.sellahi@gmail.com>
2018-08-08 08:20:58 +0200

    Identity revisited init p306

---
+++ Users/Users/Models/AppIdentityContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Users.Models
+{
+   public class AppIdentityContext:IdentityDbContext<AppUser>
+   {
+      public AppIdentityContext():base("IdentityDb")
+      {
+
+      }
+
+      /// <summary>
+      /// static constructor
+      /// </summary>
+      static AppIdentityContext()
+      {
+         Database.SetInitializer<AppIdentityContext>(new IdentityDbInit()); 
+      }
+
+      /// <summary>
+      /// simple factory method for <see cref="AppIdentityContext"/>
+      /// </summary>
+      /// <returns></returns>
+      public static AppIdentityContext Create()
+      {
+         return new AppIdentityContext();
+      }
+   }
+}
